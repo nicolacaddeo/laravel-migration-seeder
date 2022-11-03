4d2ae40c82a337908fe3bc5c60b9578106af95ed
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-11-04 00:04:42 +0100

    added cards for trains

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,36 @@
 </head>
 
 <body>
+    <h2>Treni in partenza oggi:</h2>
+
+    <div class="card-container">
+        @foreach ($trainsToday as $train)
+            <div class="train-card">
+                <p>Codice treno: {{ $train->codice_treno }}</p>
+                <p>Stazione di partenza: {{ $train->stazione_di_partenza }}</p>
+                <p>Stazione di arrivo: {{ $train->stazione_di_arrivo }}</p>
+                <p>Orario di partenza: {{ $train->orario_di_partenza }}</p>
+                <p>Orario di arrivo: {{ $train->orario_di_arrivo }}</p>
+                <p>Numero carrozze: {{ $train->numero_carrozze }}</p>
+                <p>
+                    In orario:
+                    @if ($train->in_orario == true)
+                        <strong>Si</strong>
+                    @else
+                        <strong>In ritardo</strong>
+                    @endif
+                </p>
+                <p>
+                    Cancellato:
+                    @if ($train->cancellato == true)
+                        <strong>Treno soppresso</strong>
+                    @else
+                        <strong>No</strong>
+                    @endif
+                </p>
+            </div>
+        @endforeach
+    </div>
 
 </body>
 
